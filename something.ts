16943b1e1ae268b6b739e89de70122fc73b3a214
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2024-05-27 15:40:04 +0100

    Add some other stuff

--- something.ts
+++ something.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 const API_URL = "https://localhost:5001";
-const IMAGE_PATH = path.join(__dirname, "./images/blue.png");
+let color = "blue";
 
 const signalRConnection = new HubConnectionBuilder()
   .withUrl(`${API_URL}/hubs/v1/depthCameraHub`, {
@@ -19,21 +19,20 @@ signalRConnection.start().then(async () => {
   try {
     console.log("Connected to signalR");
     // Read the image file from the file system
+    setInterval(async () => {
+      const buffer = getImageAsBuffer();
+      const base64Image = getImage(buffer);
+      color = color === "blue" ? "yellow" : "blue";
+      console.log(color);
 
-    const imageBuffer = fs.readFileSync(IMAGE_PATH);
-    console.log('I get here')
-    // Convert the image data to Base64
-    const base64Image = imageBuffer.toString("base64");
-    console .log('I get here')
-
-    // Send the Base64 encoded image through SignalR
-    await signalRConnection.invoke("PiOffersStream", base64Image);
-    console.log("Image sent to SignalR");
+      // Send the Base64 encoded image through SignalR
+      await signalRConnection.invoke("PiOffersStream", base64Image);
+      await signalRConnection.invoke("ImageAsBuffer", buffer);
+      console.log("Image sent to SignalR");
+    }, 30);
   } catch (error) {
     console.error("Error reading or sending image:", error);
   }
-
-  
 });
 
 signalRConnection.on(`BrowserRequiresStream`, async () => {
@@ -41,3 +40,20 @@ signalRConnection.on(`BrowserRequiresStream`, async () => {
 
   // Get an image from this server, send it to signalR
 });
+
+signalRConnection.on("NewImage", async (image) => {
+    console.log('the image', image);
+    const imageTag = `<img src="data:image/png;base64,${image}" alt="Appended Image">`;
+    fs.appendFile('./test.html', imageTag, () => {
+        console.log('error');
+    });
+})
+
+const getImage = (imageBuffer: Buffer) => {
+  // const imageBuffer = fs.readFileSync(path.join(__dirname, `./images/${color}.png`));
+  return imageBuffer.toString("base64");
+};
+
+const getImageAsBuffer = () => {
+  return fs.readFileSync(path.join(__dirname, `./images/${color}.png`));
+}
