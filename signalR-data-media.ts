d15575569294b6c26c9eac0c9e3cd6c31d3ec98d
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2024-06-18 23:34:20 +0100

    Actual url

--- signalR-data-media.ts
+++ signalR-data-media.ts
@@ -9,8 +9,8 @@ import {
 } from "wrtc";
 import { Blob } from "node:buffer";
 
-// const API_URL = "https://dev-api-vpc.egoscue.com";
-const API_URL = "https://localhost:5001";
+const API_URL = "https://dev-api-vpc.egoscue.com";
+// const API_URL = "https://localhost:5001";
 
 const CAMERA_API_URL = "https://localhost";
 
